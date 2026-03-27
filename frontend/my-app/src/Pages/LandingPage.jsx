84d5f1e8ea41d89a266fa274b97fad142d0deac2
sridharabhishek7@gmail.com <abhishek.kalakbandi-sridhar@stud.th-deg.de>
2026-03-27 21:16:40 +0100

    Initialized Features Section

--- frontend/my-app/src/Pages/LandingPage.jsx
+++ frontend/my-app/src/Pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/layout/Navbar'
 import Hero from '../components/landing/Hero'
+import Features from '../components/landing/Features'
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Features/>
     </div>
   )
 }
