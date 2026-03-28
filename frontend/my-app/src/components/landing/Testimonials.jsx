68c85d07e88d82866f6f2b4e9a21812fc6bf2360
sridharabhishek7@gmail.com <abhishek.kalakbandi-sridhar@stud.th-deg.de>
2026-03-28 13:39:28 +0100

    Structurized the testimonial section in Landing Page

--- frontend/my-app/src/components/landing/Testimonials.jsx
+++ frontend/my-app/src/components/landing/Testimonials.jsx
@@ -4,7 +4,93 @@ import {Star,Quote} from 'lucide-react'
 
 const Testimonials = () => {
   return (
-    <div id='testimonials' className=''></div>
+    <div id='testimonials' className=''>
+      {/*Decorative Elements*/}
+      <div className=''></div>
+      <div className=''></div>
+
+      <div className=''>
+        {/*Header*/}
+        <div className=''>
+          <div className=''>
+            <Star className=''/>
+            <span className=''>Testimonials</span>
+          </div>
+          <h2 className=''>
+            Loved by Creators
+            <span className=''>Everywhere</span>
+          </h2>
+          <p className=''>
+            Here's what the users have to say about our app
+          </p>
+        </div>
+
+        {/*Testimonials Grid*/}
+        <div className=''>
+          {TESTIMONIALS.map((testimonial, index)=>{
+            <div
+              key={index}
+              className=''
+            >
+              {/*Quote Icon*/}
+              <div className=''>
+                <Quote className=''/>  
+              </div>
+
+              {/*Rating Stars*/}
+              <div className=''>
+                {[...Array(testimonial.rating)].map((_,i)=>{
+                  <Star
+                    key={i}
+                    className=''
+                  />
+                })}  
+              </div>
+
+              {/*Quote*/}
+              <p className=''>
+                "{testimonial.quote}"  
+              </p>
+
+              {/*Author Info*/}
+              <div className=''>
+                <div className=''>
+                  <div className=''></div>
+                  <img 
+                    src="{testimonal.avatar}" 
+                    alt="{testimonial.author}"
+                    className='' 
+                  />
+                </div>
+                <div className=''>
+                  <p className=''>{testimonial.author}</p>
+                  <p className=''>{testimonial.title}</p>
+                </div>  
+              </div>
+
+              {/*Hover Gradient Background*/}
+              <div className=''></div> 
+            </div>
+          })}
+        </div>
+
+        {/*Bottom Stats*/}
+        <div className=''>
+          <div className=''>
+            <div className=''>50K+</div>
+            <div className=''>Happy Customers</div>
+          </div>
+          <div className=''>
+            <div className=''>4.9/5</div>
+            <div className=''>Average Rating</div>
+          </div>
+          <div className=''>
+            <div className=''>100K+</div>
+            <div className=''>Ebooks Created</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
